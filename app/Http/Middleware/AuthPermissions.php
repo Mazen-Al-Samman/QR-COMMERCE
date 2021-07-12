6d72f9b0f2ce78e16ea6ddc8bd9be10398da4ff3
Mazen-Al-Samman <mazenalsmman@gmail.com>
2021-07-12 23:23:48 +0300

    Create a middleware for permissions

---
+++ app/Http/Middleware/AuthPermissions.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\RolePermission;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+class AuthPermissions
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $currentAction = Route::current()->getName();
+        $permissions = RolePermission::select("permission")->where(['role_id' => Auth::user()->role_id])->join('permissions', 'permissions.id', '=', 'role_permissions.permission_id')->get();
+        if (!$this->hasPermission($permissions, $currentAction)) {
+            return redirect()->route('dashboard');
+        }
+        return $next($request);
+    }
+
+    public function hasPermission($permissions, $current_action) {
+        foreach ($permissions as $permission) {
+            if ($permission->permission == $current_action) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -22,53 +22,54 @@ Route::get('/', function () {
 
 Route::prefix('backend')->group(function () {
     Auth::routes();
-    Route::group(['middleware' => ['auth','prevent-back-history']], function () {
+    Route::group(['middleware' => ['auth', 'prevent-back-history']], function () {
         Route::get('/', [\App\Http\Controllers\AdminController::class, 'index'])->name('dashboard');
-        Route::get('/profile', [\App\Http\Controllers\MainController::class, 'profile'])->name('profile');
-        Route::put('/update/profile', [\App\Http\Controllers\MainController::class, 'updateProfile'])->name('update.profile');
+        Route::group(['middleware' => ['auth-permissions']], function () {
+            Route::get('/profile', [\App\Http\Controllers\MainController::class, 'profile'])->name('profile');
+            Route::put('/update/profile', [\App\Http\Controllers\MainController::class, 'updateProfile'])->name('update.profile');
 
-        Route::prefix('admin')->group(function () {
-            Route::get('/', [\App\Http\Controllers\AdminController::class, 'create'])->name('admin.create');
-            Route::post('/store', [\App\Http\Controllers\AdminController::class, 'store'])->name('admin.store');
-            Route::delete('/delete/{id}', [\App\Http\Controllers\AdminController::class, 'destroy'])->name('admin.delete');
-            Route::put('/update/{id}', [\App\Http\Controllers\AdminController::class, 'update'])->name('admin.update');
-            Route::get('/edit/{id}', [\App\Http\Controllers\AdminController::class, 'edit'])->name('admin.edit');
-            Route::get('/show/{id}', [\App\Http\Controllers\AdminController::class, 'show'])->name('admin.show');
-        });
+            Route::prefix('admin')->group(function () {
+                Route::get('/', [\App\Http\Controllers\AdminController::class, 'create'])->name('admin.create');
+                Route::post('/store', [\App\Http\Controllers\AdminController::class, 'store'])->name('admin.store');
+                Route::delete('/delete/{id}', [\App\Http\Controllers\AdminController::class, 'destroy'])->name('admin.delete');
+                Route::put('/update/{id}', [\App\Http\Controllers\AdminController::class, 'update'])->name('admin.update');
+                Route::get('/edit/{id}', [\App\Http\Controllers\AdminController::class, 'edit'])->name('admin.edit');
+                Route::get('/show/{id}', [\App\Http\Controllers\AdminController::class, 'show'])->name('admin.show');
+            });
 
-        Route::prefix('role')->group(function () {
-            Route::get('/', [\App\Http\Controllers\RoleController::class, 'create'])->name('role.create');
-            Route::post('/store', [\App\Http\Controllers\RoleController::class, 'store'])->name('role.store');
-            Route::delete('/delete/{id}', [\App\Http\Controllers\RoleController::class, 'destroy'])->name('role.delete');
-            Route::put('/update/{id}', [\App\Http\Controllers\RoleController::class, 'update'])->name('role.update');
-            Route::get('/edit/{id}', [\App\Http\Controllers\RoleController::class, 'edit'])->name('role.edit');
-            Route::get('/show/{id}', [\App\Http\Controllers\RoleController::class, 'show'])->name('role.show');
-        });
+            Route::prefix('role')->group(function () {
+                Route::get('/', [\App\Http\Controllers\RoleController::class, 'create'])->name('role.create');
+                Route::post('/store', [\App\Http\Controllers\RoleController::class, 'store'])->name('role.store');
+                Route::delete('/delete/{id}', [\App\Http\Controllers\RoleController::class, 'destroy'])->name('role.delete');
+                Route::put('/update/{id}', [\App\Http\Controllers\RoleController::class, 'update'])->name('role.update');
+                Route::get('/edit/{id}', [\App\Http\Controllers\RoleController::class, 'edit'])->name('role.edit');
+                Route::get('/show/{id}', [\App\Http\Controllers\RoleController::class, 'show'])->name('role.show');
+            });
 
-        Route::prefix('permission')->group(function () {
-            Route::get('/', [\App\Http\Controllers\PermissionController::class, 'create'])->name('permission.create');
-            Route::post('/store', [\App\Http\Controllers\PermissionController::class, 'store'])->name('permission.store');
-            Route::delete('/delete/{id}', [\App\Http\Controllers\PermissionController::class, 'destroy'])->name('permission.delete');
-            Route::put('/update/{id}', [\App\Http\Controllers\PermissionController::class, 'update'])->name('permission.update');
-            Route::get('/edit/{id}', [\App\Http\Controllers\PermissionController::class, 'edit'])->name('permission.edit');
-            Route::get('/show/{id}', [\App\Http\Controllers\PermissionController::class, 'show'])->name('permission.show');
-        });
+            Route::prefix('permission')->group(function () {
+                Route::get('/', [\App\Http\Controllers\PermissionController::class, 'create'])->name('permission.create');
+                Route::post('/store', [\App\Http\Controllers\PermissionController::class, 'store'])->name('permission.store');
+                Route::delete('/delete/{id}', [\App\Http\Controllers\PermissionController::class, 'destroy'])->name('permission.delete');
+                Route::put('/update/{id}', [\App\Http\Controllers\PermissionController::class, 'update'])->name('permission.update');
+                Route::get('/edit/{id}', [\App\Http\Controllers\PermissionController::class, 'edit'])->name('permission.edit');
+                Route::get('/show/{id}', [\App\Http\Controllers\PermissionController::class, 'show'])->name('permission.show');
+            });
 
-        Route::prefix('rolePermission')->group(function () {
-            Route::get('/', [\App\Http\Controllers\RolePermissionController::class, 'index'])->name('rolePermission.index');
-            Route::get('/manage/{role_id}', [\App\Http\Controllers\RolePermissionController::class, 'edit'])->name('rolePermission.manage');
-            Route::put('/update/{role_id}', [\App\Http\Controllers\RolePermissionController::class, 'update'])->name('rolePermission.update');
-            Route::get('/show/{role_id}', [\App\Http\Controllers\RolePermissionController::class, 'show'])->name('rolePermission.show');
-        });
+            Route::prefix('rolePermission')->group(function () {
+                Route::get('/', [\App\Http\Controllers\RolePermissionController::class, 'index'])->name('rolePermission.index');
+                Route::get('/manage/{role_id}', [\App\Http\Controllers\RolePermissionController::class, 'edit'])->name('rolePermission.manage');
+                Route::put('/update/{role_id}', [\App\Http\Controllers\RolePermissionController::class, 'update'])->name('rolePermission.update');
+                Route::get('/show/{role_id}', [\App\Http\Controllers\RolePermissionController::class, 'show'])->name('rolePermission.show');
+            });
 
-        Route::prefix('vendor')->group(function () {
-            Route::get('/', [\App\Http\Controllers\VendorsController::class, 'create'])->name('vendor.create');
-            Route::post('/store', [\App\Http\Controllers\VendorsController::class, 'store'])->name('vendor.store');
-            Route::delete('/delete/{id}', [\App\Http\Controllers\VendorsController::class, 'destroy'])->name('vendor.delete');
-            Route::get('/edit/{id}', [\App\Http\Controllers\VendorsController::class, 'edit'])->name('vendor.edit');
-            Route::get('/show/{id}', [\App\Http\Controllers\VendorsController::class, 'show'])->name('vendor.show');
-            Route::put('/update/{id}', [\App\Http\Controllers\VendorsController::class, 'update'])->name('vendor.update');
+            Route::prefix('vendor')->group(function () {
+                Route::get('/', [\App\Http\Controllers\VendorsController::class, 'create'])->name('vendor.create');
+                Route::post('/store', [\App\Http\Controllers\VendorsController::class, 'store'])->name('vendor.store');
+                Route::delete('/delete/{id}', [\App\Http\Controllers\VendorsController::class, 'destroy'])->name('vendor.delete');
+                Route::get('/edit/{id}', [\App\Http\Controllers\VendorsController::class, 'edit'])->name('vendor.edit');
+                Route::get('/show/{id}', [\App\Http\Controllers\VendorsController::class, 'show'])->name('vendor.show');
+                Route::put('/update/{id}', [\App\Http\Controllers\VendorsController::class, 'update'])->name('vendor.update');
+            });
         });
     });
-
 });
