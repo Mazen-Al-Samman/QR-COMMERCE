ec2d33e667dcc36b133416b322d687d26ef2351f
abedghr <abedg@motory.com>
2021-07-17 01:18:41 +0300

    typo

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -12,7 +12,7 @@ class Permission extends Model
 
     public function getAllPermissions()
     {
-        return Permission::paginate(1);
+        return Permission::paginate(15);
     }
 
     public function getPermissionsWithSelected($role_id)
